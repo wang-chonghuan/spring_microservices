8f1ea4593a7e0b8f8283c4e103375c0b1acc73bb
wang-chonghuan <wangchonghuan@gmail.com>
2022-11-26 16:48:38 +0000

    add TeacherMicroservice

---
+++ TeacherMicroservice/src/main/java/com/wang/TeacherMicroservice/TeacherMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wang.TeacherMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherMicroserviceApplication.class, args);
+	}
+
+}
---
+++ TeacherMicroservice/src/test/java/com/wang/TeacherMicroservice/TeacherMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.TeacherMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
