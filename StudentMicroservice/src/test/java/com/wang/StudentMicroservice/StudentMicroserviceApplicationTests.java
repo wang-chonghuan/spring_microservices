956286819e12c8279e2aaa16e68f8fde99b3f006
wang-chonghuan <wangchonghuan@gmail.com>
2022-11-26 16:18:40 +0000

    add StudentMsrv boilplate

---
+++ StudentMicroservice/src/test/java/com/wang/StudentMicroservice/StudentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.StudentMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
