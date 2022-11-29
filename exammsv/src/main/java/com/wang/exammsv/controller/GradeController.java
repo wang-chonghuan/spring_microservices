5c0537834a29cffc12ea3e9a9319a21d049da0a1
wang-chonghuan <wangchonghuan@gmail.com>
2022-11-29 01:17:13 +0000

    controller

--- exammsv/src/main/java/com/wang/exammsv/controller/GradeController.java
+++ exammsv/src/main/java/com/wang/exammsv/controller/GradeController.java
@@ -1,5 +1,8 @@
 package com.wang.exammsv.controller;
 
+import com.wang.exammsv.domain.Blankpaper;
+import com.wang.exammsv.domain.StudentExamResult;
+import com.wang.exammsv.repository.StudentExamResultRepository;
 import com.wang.exammsv.service.GradeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,10 +21,14 @@ public class GradeController {
 
     @Autowired
     private GradeService gradeService;
+    @Autowired
+    private StudentExamResultRepository resultRepository;
 
     @RequestMapping(value="/viewanswers", method= RequestMethod.GET)
     public ResponseEntity<?> viewAnswers(@RequestParam long studentId, @RequestParam long examId) throws Exception {
-        return ResponseEntity.ok().body("");
+        Optional<StudentExamResult> result = resultRepository.findByStudentIdAndExamId(studentId, examId).stream().findFirst();
+        var ret = result.get().getAnsweredpaper();
+        return ResponseEntity.ok().body(result.get().getAnsweredpaper());
     }
 
     @RequestMapping(value="/manual", method= RequestMethod.POST)
